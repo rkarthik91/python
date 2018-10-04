4be39ef10100843ca839576ced3116510f1ab924
rkarthik91 <karthikramanathan39@gmail.com>
2018-10-04 18:46:57 +0530

    commit

---
+++ arithmetic operations/addition of two numbers.py
@@ -0,0 +1,11 @@
+
+# addition of two numbers
+
+a=int(input("Enter the first number")) # getting the first input from the user
+
+b=int(input("Enter the second input")) # getting the secod input from the user
+# since the input () function returns string it has to be type casted to integer
+
+c=a+b 
+
+print("Addition of aa and b is ",c)
